5ecf7d6e7a930ccb742f23c779e762127f44d358
mwalczak36 <mikewalczak36@gmail.com>
2024-10-02 20:12:10 -0400

    Update main.py
    Changed order of booleans

--- Assignments/Password Validator/main.py
+++ Assignments/Password Validator/main.py
@@ -1,37 +1,37 @@
 #Date: 9/30/24
 #Author: Michael Walczak
 #Password Validator
+def main():
+    sName = input("Enter first and last name: ").title() #1
+    #sName = "michael walczak".title()
+    sInitials = sName[0] + sName[sName.find(" ") + 1] #4
+    sPassword = ""
+    bIsValid = False
+    while not bIsValid: #2
+        bLength = bPass = bInitials = bUpper = bLower = bDigit = bSpecial = bCount = False
+        dictCharacters = {}
+        sPassword = input("Enter Password: ") #3
+        #if not sPassword: break
 
-sName = input("Enter first and last name: ").title() #1
-#sName = "michael walczak".title()
-sInitials = sName[0] + sName[sName.find(" ") + 1] #4
-sPassword = ""
+        bLength = True if len(sPassword) in range(8, 13) else print("Password must be between 8 and 12 characters.") #5
+        bPass = True if not sPassword.lower().startswith("pass") else print("Password can't start with Pass") #6
+        bInitials = True if not sInitials.lower() in sPassword.lower() else print("Password must not contain user initials") #11
 
-bIsValid = bLength = bPass = bInitials = bUpper = bLower = bDigit = bSpecial = bCount = False
+        for char in sPassword:
+            if char.isupper():   bUpper = True
+            if char.islower():   bLower = True
+            if char.isdigit():   bDigit = True
+            if char in "!@#$%^": bSpecial = True
+            iCount = sPassword.count(char.lower())
+            if iCount > 1: dictCharacters[char.lower()] = iCount
 
-while not bIsValid: #2
-    dictCharacters = {}
-    sPassword = input("Enter Password: ") #3
-    if not sPassword: break
+        if not bUpper: print("Password must contain at least 1 uppercase letter.") #7
+        if not bLower: print("Password must contain at least 1 lowercase letter.") #8
+        if not bDigit: print("Password must contain at least 1 number.") #9
+        if not bSpecial: print("Password must contain at least 1 of these special characters: ! @ # $ % ^ ") #10
+        bCount = True if not dictCharacters else print(f"These characters occur twice:\n{dictCharacters}") #12
 
-    bLength = True if len(sPassword) in range(8, 13) else print("Password must be between 8 and 12 characters.") #5
-    bPass = True if not sPassword.lower().startswith("pass") else print("Password can't start with Pass") #6
-    bInitials = True if not sInitials.lower() in sPassword.lower() else print("Password must not contain user initials") #11
-
-    for char in sPassword:
-        if char.isupper():   bUpper = True
-        if char.islower():   bLower = True
-        if char.isdigit():   bDigit = True
-        if char in "!@#$%^": bSpecial = True
-        iCount = sPassword.lower().count(char)
-        if iCount > 1: dictCharacters[char.lower()] = iCount
-
-    if not bUpper: print("Password must contain at least 1 uppercase letter.") #7
-    if not bLower: print("Password must contain at least 1 lowercase letter.") #8
-    if not bDigit: print("Password must contain at least 1 number.") #9
-    if not bSpecial: print("Password must contain at least 1 of these special characters: ! @ # $ % ^ ") #10
-    bCount = True if not bool(dictCharacters) else print(f"These characters occur twice:\n{dictCharacters}") #12
-
-    if bLength and bPass and bInitials and bUpper and bLower and bDigit and bSpecial and bCount: bIsValid = True
-print(f"\nName: {sName}\nInitails: {sInitials}\nPassword: {sPassword}")
-print("Password is valid and OK to use\n")
+        if bLength and bPass and bInitials and bUpper and bLower and bDigit and bSpecial and bCount: bIsValid = True
+    print(f"\nName: {sName}\nInitails: {sInitials}\nPassword: {sPassword}")
+    print("Password is valid and OK to use\n")
+main()
