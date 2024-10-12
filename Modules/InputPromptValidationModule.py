fadef523d84f593ed174df9915cfa067af36adb1
mwalczak36 <mikewalczak36@gmail.com>
2024-10-12 17:24:28 -0400

    Update InputPromptValidationModule.py
    updated syntax for better readability

--- Modules/InputPromptValidationModule.py
+++ Modules/InputPromptValidationModule.py
@@ -4,8 +4,6 @@
 def validateNumericInput(sPrompt:str, bAllowZero:bool = False)->tuple[float, bool]:
     fNumber = -1
     while (bAllowZero and fNumber < 0) or (not bAllowZero and fNumber <= 0):
-        try:
-            fNumber = float(input(sPrompt))
-        except ValueError:
-            print("Invalid input.")
+        try: fNumber = float(input(sPrompt))
+        except ValueError: print("Invalid input.")
     return (fNumber, bAllowZero)
