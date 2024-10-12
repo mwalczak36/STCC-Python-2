9927485c72862655584ec66903013ead7b37e89e
mwalczak36 <mikewalczak36@gmail.com>
2024-10-12 15:16:59 -0400

    Create InputPromptValidationModule.py
    module created for validating input into a floating point number with optionally allowing for zero

---
+++ Modules/InputPromptValidationModule.py
@@ -0,0 +1,11 @@
+#Date: 10/12/24
+#Author: Michael Walczak
+#Input Prompt Validation Module
+def validateNumericInput(sPrompt:str, bAllowZero:bool = False)->tuple[float, bool]:
+    fNumber = -1
+    while (bAllowZero and fNumber < 0) or (not bAllowZero and fNumber <= 0):
+        try:
+            fNumber = float(input(sPrompt))
+        except ValueError:
+            print("Invalid input.")
+    return (fNumber, bAllowZero)
