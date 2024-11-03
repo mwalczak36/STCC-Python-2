5459fd5966f73c24b50a9b0e034bef735db73ee8
mwalczak36 <mikewalczak36@gmail.com>
2024-11-02 22:12:38 -0400

    Update RealEstateAnalyzerModule.py
    updated annotations, and f string formatting

--- Assignments/Real Estate Analyzer/RealEstateAnalyzerModule.py
+++ Assignments/Real Estate Analyzer/RealEstateAnalyzerModule.py
@@ -4,7 +4,7 @@
 
 import csv
 
-def getDataInput(sFileName:str)->tuple[list, list]:
+def getDataInput(sFileName:str)->tuple[list, list[list]]:
     with open(sFileName, "r") as file:
         reader = csv.reader(file)
         lstHeader = next(reader)
@@ -12,36 +12,31 @@ def getDataInput(sFileName:str)->tuple[list, list]:
     return (lstHeader, lstData)
 
 def main():
+    
     lstHeader, lstData = getDataInput("RealEstateData.csv")
-
     lstCitys, lstPropertyTypes, lstPrices, dictCitys, dictZips, dictPropertyTypes = [], [], [], {}, {}, {}
-   
+
     for row in lstData:
-
-        sCity = row[1]
-        sZip = row[2]
-        sPropertyType = row[7]
-        fPrice = float(row[8])
-
+        sCity, sZip, sPropertyType, fPrice = row[1], row[2], row[7], float(row[8])
         if sCity not in lstCitys: lstCitys.append(sCity)
         if sPropertyType not in lstPropertyTypes: lstPropertyTypes.append(sPropertyType)
         lstPrices.append(fPrice)
-
         dictCitys[sCity] = dictCitys.get(sCity, 0) + fPrice
         dictZips[sZip] = dictZips.get(sZip, 0) + fPrice
         dictPropertyTypes[sPropertyType] = dictPropertyTypes.get(sPropertyType, 0) + fPrice
 
-    print(f"\n{'Minimum':15s}{min(lstPrices):15,.2f}")
-    print(f"{'Maximum':15s}{max(lstPrices):15,.2f}")
-    print(f"{'Sum':15s}{sum(lstPrices):15,.2f}")
-    print(f"{'Avg':15s}{sum(lstPrices)/len(lstPrices):15,.2f}")
-    print(f"{'Median':15s}{(lambda lst: (sorted(lst)[len(lst) // 2] if len(lst) % 2 != 0 else (sorted(lst)[len(lst) // 2] + sorted(lst)[len(lst) // 2 - 1]) / 2)) (lstPrices):15,.2f}")
-
+    print(f"\n{'Minimum':20s}{min(lstPrices):15,.2f}")
+    print(f"{'Maximum':20s}{max(lstPrices):15,.2f}")
+    print(f"{'Sum':20s}{sum(lstPrices):15,.2f}")
+    print(f"{'Avg':20s}{sum(lstPrices)/len(lstPrices):15,.2f}")
+    print(f"{'Median':20s}{(lambda lst: (sorted(lst)[len(lst) // 2] if len(lst) % 2 != 0 else (sorted(lst)[len(lst) // 2] + sorted(lst)[len(lst) // 2 - 1]) / 2)) (lstPrices):15,.2f}")
+    
     print("\nSummary by Property Type:")
-    for propertyType, fTotal in dictPropertyTypes.items(): print(f"{propertyType:15s}{fTotal:15,.2f}")
-    print()
-
-    for sCity, fTotal in dictCitys.items(): print(f"{sCity:15s}{fTotal:15,.2f}")
-    print()
-
+    for propertyType, fTotal in dictPropertyTypes.items(): print(f"{propertyType:20s}{fTotal:15,.2f}")
+    
+    print("\nSummary by City:")
+    for sCity, fTotal in dictCitys.items(): print(f"{sCity:20s}{fTotal:15,.2f}")
+    
+    print("\nSummary by Zips:")
+    for sZip, fTotal in dictZips.items(): print(f"{sZip:20s}{fTotal:15,.2f}")
 main()
